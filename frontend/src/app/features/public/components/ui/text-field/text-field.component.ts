93d543409e7c8eb3427381d5e8db10eada2843e7
robert <78978033+robertGGA@users.noreply.github.com>
2024-01-17 23:17:38 +0300

    feat(F-02): added base of dialog (#6)

---
+++ frontend/src/app/features/public/components/ui/text-field/text-field.component.ts
@@ -0,0 +1,14 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+    selector: 'rg-text-field',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './text-field.component.html',
+    styleUrls: ['./text-field.component.sass'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TextFieldComponent {
+
+}
--- frontend/src/app/features/public/pages/login-page/login-page.component.ts
+++ frontend/src/app/features/public/pages/login-page/login-page.component.ts
@@ -1,12 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DefaultDialogComponent} from "@features/public/components/dialog/default-dialog/default-dialog.component";
 
 @Component({
     selector: 'rg-login-page',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, DefaultDialogComponent],
     templateUrl: './login-page.component.html',
     styleUrls: ['./login-page.component.sass'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+}
